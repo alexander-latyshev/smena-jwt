cf59861c7f5a36df2be5fa13a549eb976fbf4cb9
Alexander Latyshev <aladdelinxr@gmail.com>
2022-11-30 21:55:42 +0300

    Add layout

---
+++ src/components/auth/login.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Form, Input } from "antd";
+import { Link } from "react-router-dom";
+
+type Props = {};
+
+const Login = (props: Props) => {
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      style={{ marginTop: 50, width: 400 }}
+    >
+      <Form.Item
+        name="username"
+        rules={[{ required: true, message: "Please input your Username!" }]}
+      >
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder="Username"
+        />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: "Please input your Password!" }]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+        Or <Link to={"/register"}>register now!</Link>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Login;
