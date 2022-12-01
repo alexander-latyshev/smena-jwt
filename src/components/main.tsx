7fd2c24d33e48f07780f40475d778977f791985d
Alexander Latyshev <aladdelinxr@gmail.com>
2022-12-01 17:28:16 +0300

    Add navigation

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import About from "./auth/about";
 import Login from "./auth/login";
@@ -8,6 +8,14 @@ import Register from "./auth/register";
 type Props = {};
 
 const Main = (props: Props) => {
+  const pathname: string = window.location.pathname;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (pathname === "/") navigate("/register");
+    console.log(1);
+  }, [pathname]);
+
   return (
     <div className="main" style={{ padding: "auto" }}>
       <Routes>
